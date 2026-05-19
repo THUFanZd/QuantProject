973f7bdcbfcf0c0645a1f7e3b0c57dceff30e09c
grapsss <gr25@mails.tsinghua.edu.cn>
2026-05-19 22:02:28 +0800

    refactor: simplify factor library exports

--- group_work/factor_lib/__init__.py
+++ group_work/factor_lib/__init__.py
@@ -1,19 +1,5 @@
 """Reusable helpers for the course factor pipeline."""
 
-from .factors import (
-    FACTOR_REGISTRY,
-    factor_05_volume_price_divergence_cov,
-    factor_08_liquidity_stability,
-    factor_35_price_spread_momentum,
-    factor_38_volatility_difference_proxy,
-    factor_42_adjusted_price_reversal,
-)
+from .factors import FACTOR_REGISTRY
 
-__all__ = [
-    "FACTOR_REGISTRY",
-    "factor_05_volume_price_divergence_cov",
-    "factor_08_liquidity_stability",
-    "factor_35_price_spread_momentum",
-    "factor_38_volatility_difference_proxy",
-    "factor_42_adjusted_price_reversal",
-]
+__all__ = ["FACTOR_REGISTRY"]
